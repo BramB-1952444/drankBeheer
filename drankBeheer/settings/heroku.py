75c24dbb31dac07866e1b3acb3f7c0383b18cda4
8ram <bram.boeckx.bb@gmail.com>
2021-02-09 10:04:28 +0100

    updated heroku settings

--- drankBeheer/settings/heroku.py
+++ drankBeheer/settings/heroku.py
@@ -1,10 +1,16 @@
+"""
+Production Settings for Heroku
+"""
+
 import environ
+
+# If using in your own project, update the project namespace below
+from gettingstarted.settings.base import *
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-# reading .env file
-environ.Env.read_env()
 
 # False if not in os.environ
 DEBUG = env('DEBUG')
@@ -12,17 +18,10 @@ DEBUG = env('DEBUG')
 # Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
 SECRET_KEY = env('SECRET_KEY')
 
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
 # Parse database connection url strings like psql://user:pass@127.0.0.1:8458/db
 DATABASES = {
     # read os.environ['DATABASE_URL'] and raises ImproperlyConfigured exception if not found
     'default': env.db(),
-    # read os.environ['SQLITE_URL']
-    'extra': env.db('SQLITE_URL', default='sqlite:////tmp/my-tmp-sqlite.db')
-}
-
-CACHES = {
-    # read os.environ['CACHE_URL'] and raises ImproperlyConfigured exception if not found
-    'default': env.cache(),
-    # read os.environ['REDIS_URL']
-    'redis': env.cache('REDIS_URL')
 }
